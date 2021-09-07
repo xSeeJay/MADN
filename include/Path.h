14d028fd0363d88bd5208807dd93ed8bb5aab049
SEExJAY <jasongloger123@googlemail.com>
2021-09-07 21:00:22 +0200

    Change comment of class!

--- include/Path.h
+++ include/Path.h
@@ -6,21 +6,20 @@
 #include <QPoint>
 #include <QVector>
 
-/* This class provides a way to know where exactly on the board a pawn should be
- * placed. It converts a pawn's relative position to cordinates on the board grid
- * NOTE: This class does not computes anything but rather has all the cordinates
- * pre-stored as a static member which is used to return the cordinates */
+/* Diese Klasse zeigt wo genau auf dem Spielfeld eine Spielfigur sein sollte.
+ * Es wandelt die relative Position einer Spielfigur auf dem Spielfeld um.
+ * Die Klasse besitzt nur alle Koordinaten. */
 class Path {
 public:
-    static const unsigned int MAX_REL; //The maximum value relative position a pawn can have
+    static const unsigned int MAX_REL; //Der maximale Wert, den eine Spielfigur haben kann
 
-    /*Returns the pawn's cordinates on the board grid using it's relative position*/
+    //Gibt die Spielfigurkoordinaten auf dem Spielfeld zurück mithilfe der relativen Position
     static QPoint getGenaueKoordinaten(unsigned int rel);
     static QPoint rotatePointToRight(QPoint p);
     static QPoint rotatePointToRight(QPoint p, int times);
 private:
-    static QVector<QPoint> path; //Data structure to store the coordinates
-    static const int TOP = 14; //Total number of cells in a row (or colomn)
+    static QVector<QPoint> path; //Hier werden die Koordinaten gespeichert
+    static const int TOP = 14; //Insgesamte Anzahl von Felder in einer Reihe
 };
 
 #endif //Path.h
